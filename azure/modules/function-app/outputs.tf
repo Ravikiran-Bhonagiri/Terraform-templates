41199c52fc85d2ecbdd1f5d42ef3ececfad060ab
Ravikiran Bhonagiri <bhonagiri.ravikiran@gmail.com>
2025-02-12 12:33:33 -0600

    Update outputs.tf

--- azure/modules/function-app/outputs.tf
+++ azure/modules/function-app/outputs.tf
@@ -1,1 +1,15 @@
+output "function_app_name" {
+  value = azurerm_linux_function_app.main.name
+}
 
+output "function_app_id" {
+  value = azurerm_linux_function_app.main.id
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.function.name
+}
+
+output "service_plan_id" {
+  value = azurerm_service_plan.main.id
+}
